d8ba73c371257beb327252290b99107ac5034434
Anish Gupta <guptaanish978@gmail.com>
2024-10-07 13:25:33 +0530

    Update textForm.js
    Add some debuges

--- src/components/textForm.js
+++ src/components/textForm.js
@@ -20,6 +20,7 @@ export default function (props) {
         let text = document.getElementById("myText");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
     }
     const handleOnChange = (event) => {
         console.log("clicked")
@@ -35,14 +36,16 @@ export default function (props) {
           <textarea className="form-control" value={text} onChange={handleOnChange} id="myText" rows={6}/>
         </div>
         
-        <button className="btn btn-success" onClick={handleUpClick}>upperCase</button>
-        <button className="btn btn-warning mx-4" onClick={handlelowerClick}>lowerCase</button>
-        <button className="btn btn-danger" onClick={handleremoveClick}>Clear</button>
-        <button className="btn btn-primary mx-4" onClick={handleCopy}>Copy</button>
+        <button disabled={text.length===0} className="btn btn-success mx-1 my-1" onClick={handleUpClick}>upperCase</button>
+        <button disabled={text.length===0} className="btn btn-warning mx-1 my-1" onClick={handlelowerClick}>lowerCase</button>
+        <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleremoveClick}>Clear</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy</button>
       </div>
       <div className={`container text-${props.mode==='light'?'dark':'light'}`}>
         <h3><b>Your Summary</b></h3>
-        <p><b> {text.split(" ").length} - Words and _ {text.length} - Characters _ {0.008 * text.split(" ").length} - Minutes take to read </b></p>
+        <p><b> 
+          {text.split(" ").filter((element) =>{return element.length !== 0}).length} - Words and _ {text.length} - Characters _ {0.008 * text.split(" ").filter((element) => {return element.length !== 0}).length} - Minutes take to read 
+          </b></p>
         <h4><b>Preview</b></h4>
         <p><b>{text.length > 0 ? text : "Enter text to preview"}</b></p>
       </div>
